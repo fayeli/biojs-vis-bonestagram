ca23f817454ee13df413284516f07ba83970f2e3
Faye Li Si Fi <lis@rose-hulman.edu>
2016-05-12 22:53:59 -0400

    Added test sample image

--- main.m
+++ main.m
@@ -1,2 +1,5 @@
-img = dicomread('VOLUMEMERGE/VOLUMEMERGE/IRM DE LA COLONNE CERVICO-DORSO-LOMBAIRE/MobiView - 305/IM-0001-0001.dcm');
-imshow(img,[]);
+img_spine = dicomread('VOLUMEMERGE/VOLUMEMERGE/IRM DE LA COLONNE CERVICO-DORSO-LOMBAIRE/MobiView - 305/IM-0001-0004.dcm');
+imshow(img_spine,[]);
+
+img_skull = dicomread('MANIX/CER-CT/ANGIO CT/IM-0001-0002.dcm');
+imshow(img_skull,[]);
