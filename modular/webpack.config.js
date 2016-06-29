27f21bace1fe70acaab3d4759ed39e5725629cf9
Faye <lis@rose-hulman.edu>
2016-06-28 18:04:06 -0700

    Fix bug with jsfeat

--- modular/webpack.config.js
+++ modular/webpack.config.js
@@ -1,17 +1,16 @@
  module.exports = {
  	resolve: {
- 	 	modulesDirectories: ['./js'],
+ 	 	modulesDirectories: ['./js','./node_modules'],
  	 	alias: {
  	 		"app": "app",
       		"numeric": "libs/numeric-1.2.6.min",
-      		"jsfeat": "libs/jsfeat-min",	
  	 	},
  	 },
-     entry: './js/app/bonestagram.js',
-     output: {
+    entry: './js/app/bonestagram.js',
+    output: {
          path: './bin',
          filename: 'bonestagram.bundle.js',
          libraryTarget: 'var',
          library: 'Bonestagram'
-     }
+    }
  };
