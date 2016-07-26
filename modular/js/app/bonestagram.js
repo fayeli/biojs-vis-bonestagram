1c996770a2e03b340c8eadf3819a7a0f37346434
Faye <lis@rose-hulman.edu>
2016-07-26 15:47:43 -0700

    display screenshot in a image element .bonestagram_screenshot

--- modular/js/app/bonestagram.js
+++ modular/js/app/bonestagram.js
@@ -235,15 +235,17 @@ define(['libs/clm','model/model_pca_20_svm', 'libs/face_deformer', 'libs/utils',
 			screenshot_canvas.width = width;
 			screenshot_canvas.height = height;
 			var screenshotCC = screenshot_canvas.getContext('2d');
+			// Take screenshot of video
 			screenshotCC.drawImage(vid, 0, 0, width, height);
 			facePos = ctrack.getCurrentPosition(vid);
 			if (facePos) {
 				// draw mask
 				fd.draw(facePos);
+				// Combine with the screenshot of the face mask
 				screenshotCC.drawImage(gl, 0, 0, width, height);
 			}
 			var dataURL = screenshot_canvas.toDataURL();
-			window.location = dataURL;
+			$('.bonestagram_screenshot').attr('src', dataURL);
 		},
 		setBonesCoords: function(coords){
 			bonesCoords = coords;
