dc8d3dac7575ab92286e1580db24a94fcb40b739
Faye <lis@rose-hulman.edu>
2016-08-12 16:36:05 -0700

    module exports on the web pack bundled code. use it as visualisation core

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,5 @@
 // if you don't specify a html file, the sniper will generate a div with id "rootDiv"
- /*
- 	Import jquery
- */
+
 var app = require("biojs-vis-bonestagram");
 var instance = new app({el: rootDiv, text: 'biojs'});
+app.startFace();
