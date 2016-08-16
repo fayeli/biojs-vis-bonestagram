6cca7ff881aeab0ea509a64ab17e2f07be4ec20d
Faye <lis@rose-hulman.edu>
2016-08-16 00:31:57 -0700

    debuggin cornerstone dicom loader

--- examples/dicom_example.js
+++ examples/dicom_example.js
@@ -6,12 +6,15 @@ var instance = new app({el: rootDiv, text: 'biojs'});
 // create a file input for DICOM upload
 var fileInput = document.createElement("input");
 fileInput.setAttribute("type", "file");
+fileInput.setAttribute("multiple", "false");
 rootDiv.appendChild(fileInput);
+app.enableDICOMUpload(fileInput);
 
 // create a div in the DOM for the DICOM preview
 var div = document.createElement("div");
 div.id = "dicomImage";
 rootDiv.appendChild(div);
+app.enableDICOMPreview();
 
 //rootDiv.append($('<div><div/>').attr("id", "dicomImage"));
 //app.startFace();
