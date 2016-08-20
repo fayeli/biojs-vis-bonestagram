ff87095f8398782405ead9b15fdccb6a0c95a857
Faye <lis@rose-hulman.edu>
2016-08-19 17:12:22 -0700

    visualise DICOM method

--- examples/dicom_example.js
+++ examples/dicom_example.js
@@ -3,9 +3,6 @@
 var app = require("biojs-vis-bonestagram");
 var instance = new app({text: 'biojs'});
 app.create();
-$("#start_button").click(function(){
-	app.start();
-});
 
 // select a file input for DICOM upload
 var fileInput = $("#file_input").get(0);
@@ -14,3 +11,7 @@ app.enableDICOMUpload(fileInput);
 // select a div for the DICOM preview
 var div = $("#dicomImage").get(0);
 app.enableDICOMPreview(div);
+
+$("#start_button").click(function(){
+	app.visualiseDICOM();
+});
